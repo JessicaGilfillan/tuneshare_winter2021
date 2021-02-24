e1a12fdf666eb0f8f396cf0dabc25ad650c30c43
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-02-24 16:31:42 -0500

    added delete functionality, css styles, error page, global header and footer

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TuneShare 2021</title>
-</head>
-<body>
-    <header>
-        <h1> TuneShare 2021 - Share Your Fave Tunes & Join The Community</h1>
-    </header>
+<?php require('header.php'); ?>
     <main>
         <form action="process.php" method="post">
-            <label for="fname"> First Name </label>
-            <input type="text" name="fname" id="fname">
-            <label for="lname"> Last Name </label>
-            <input type="text" name="lname" id="lname"> 
-            <label for="genre"> Favourite Genre </label>
-            <input type="text" name="genre" id="genre">
-            <label for="location"> Your Location </label>
-            <input type="text" name="location" id="location"> 
-            <label for="email"> Your Email </label>
-            <input type="email" name="email" id="email"> 
-            <label for="age"> Your Age </label>
-            <input type="number" name="age" id="age"> 
-            <label for="favsong"> What Are You Listening To Right Now?  </label>
-            <input type="text" name="favsong" id="favsong"> 
-            <input type="submit" value="submit" name="submit">
+            <div class="form-group">
+                <label for="fname"> First Name </label>
+                <input type="text" name="fname" id="fname" class="form-control">
+            </div> 
+            <div class="form-group">
+                <label for="lname"> Last Name </label>
+             <input type="text" name="lname" id="lname" class="form-control"> 
+            </div>
+            <div class="form-group">
+                <label for="genre"> Favourite Genre </label>
+                <input type="text" name="genre" id="genre" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="location"> Your Location </label>
+                <input type="text" name="location" id="location" class="form-control">
+            </div> 
+            <div class="form-group">
+             <label for="email"> Your Email </label>
+             <input type="email" name="email" id="email" class="form-control"> 
+            </div>
+            <div class="form-group">
+                <label for="age"> Your Age </label>
+                <input type="number" name="age" id="age"class="form-control">
+            </div> 
+            <div class="form-group">
+                <label for="favsong"> What Are You Listening To Right Now?  </label>
+                <input type="text" name="favsong" id="favsong" class="form-control"> 
+            </div>
+            <input type="submit" value="submit" name="submit" class="btn btn-primary">
         </form>
     </main>
-    <footer>
-        <p> &copy; <?php echo getdate()['year']; ?> </p>
-    </footer>
-    
-</body>
-</html>
+    <?php require('footer.php'); ?>
--- process.php
+++ process.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thanks For Submitting</title>
-</head>
-<body>
-    <header>
-        <h1> TuneShare 2021 - Share Your Fave Tunes & Join The Community</h1>
-    </header>
+<?php require('header.php'); ?>
         <?php
 
         //1. create variables to store info 
@@ -73,8 +62,4 @@
 
         }
         ?>
-    <footer>
-        <p> &copy; <?php echo getdate()['year']; ?> </p>
-    </footer> 
-</body>
-</html>
+<?php require('footer.php'); ?>

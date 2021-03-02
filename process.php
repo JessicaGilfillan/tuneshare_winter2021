ac8e9bb61b08f2a8b352f2e1f45b8926090f9740
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-03-02 13:45:57 -0500

    Update process.php

--- process.php
+++ process.php
@@ -39,7 +39,7 @@
                 //set up SQL query 
 
                 if(!empty($id)) {
-                  $sql = "UPDATE SONGS SET first_name = :firstname, last_name = :lastname, genre = :genre, location = :location, email = :email, age = :age, favsong = :favsong WHERE user_id = :user_id;"; 
+                  $sql = "UPDATE songs SET first_name = :firstname, last_name = :lastname, genre = :genre, location = :location, email = :email, age = :age, favsong = :favsong WHERE user_id = :user_id;"; 
 
                 }
                 else {

71aefe265ef27d9954041ca2082e998294e6c533
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-03-09 11:05:51 -0500

    Update header.php

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>TuneShare 2021</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">

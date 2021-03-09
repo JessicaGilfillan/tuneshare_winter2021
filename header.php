2d3a7d47cd83f10d0875b6e24629fc6b31fd8e12
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2021-03-09 11:08:04 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TuneShare 2021</title>
+    <!--Boostrap CSS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
+    <!--JQuery-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- Bootstrap JavaScript -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div class="container">
-    <!-- added bootstrap navbar https://getbootstrap.com/docs/5.0/components/navbar/ -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!-- added bootstrap navbar https://getbootstrap.com/docs/5.0/components/navbar/ -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -28,6 +34,6 @@
                 </ul>
             </div>
         </nav>
-    <header>
-        <h1> TuneShare 2021 - Share Your Fave Tunes & Join The Community</h1>
-    </header>
+        <header>
+            <h1> TuneShare 2021 - Share Your Fave Tunes & Join The Community</h1>
+        </header>
